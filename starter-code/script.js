827dd57a23141146bdb76bd2621127a026a15b2a
Sergio Valencia <Streameraudio@yahoo.com>
2021-11-22 23:34:46 -0800

    figuring out fetch()

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,8 +1,36 @@
-"strict on";
 const url = "./data.json";
-
-// window.onload((e) => {});
-
+// window.onload(() => {});
+const job = [];
 fetch(url)
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    return data;
+  })
+  .then((item) => {
+    job.push(item);
+  });
+
+console.log(job);
+
+
+
+// const jobsListings = fetch(url);
+// jobsListings.then((response) => {
+//   return console.log(response.json());
+// });
+
+const joblisting = (jobs) => {
+  for (let i = 0; i < 9; i++) {
+    document.querySelector(".grid-Page1").innerHTML += `
+    <div class="grid-item">
+    <div class="logoDiv">
+    <img class="logo" src="./assets/logos/scoot.svg" />
+    </div>
+    <span class="timeJobType">5h ago ◽ Full Time</span>
+    <span class="jobTitle">Senior Software Engineer</span>
+    <span class="companyName">Scoot</span>
+    <span class="jobLocation">United Kingdom</span>
+    </div>`;
+  }
+};
+joblisting();
