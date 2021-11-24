959c7139e93e520bf8bbac9695b4cc2e42c8b33d
Sergio Valencia <Streameraudio@yahoo.com>
2021-11-23 21:53:55 -0800

    Added some mobile responsive functionality

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,36 +1,29 @@
 const url = "./data.json";
 // window.onload(() => {});
-const job = [];
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    return data;
-  })
-  .then((item) => {
-    job.push(item);
-  });
 
-console.log(job);
-
-
-
-// const jobsListings = fetch(url);
-// jobsListings.then((response) => {
-//   return console.log(response.json());
-// });
-
-const joblisting = (jobs) => {
-  for (let i = 0; i < 9; i++) {
-    document.querySelector(".grid-Page1").innerHTML += `
-    <div class="grid-item">
-    <div class="logoDiv">
-    <img class="logo" src="./assets/logos/scoot.svg" />
-    </div>
-    <span class="timeJobType">5h ago ◽ Full Time</span>
-    <span class="jobTitle">Senior Software Engineer</span>
-    <span class="companyName">Scoot</span>
-    <span class="jobLocation">United Kingdom</span>
-    </div>`;
-  }
+const jobListings = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((job) => {
+        document.querySelector(".grid-Page1").innerHTML += `
+          <div class="grid-item">
+          <div class="logoDiv" style="background-color: ${job.logoBackground}">
+          <img class="logo" src=${job.logo} />
+          </div>
+          <span class="timeJobType">${job.postedAt} ◽ ${job.contract}</span>
+          <span class="jobTitle">${job.position}</span>
+          <span class="companyName">${job.company}</span>
+          <span class="jobLocation">${job.location}</span>
+          </div>`;
+      });
+    });
 };
-joblisting();
+jobListings();
+
+const loadMore = () => {
+  document.querySelector(".grid-Page1").style.height = "100%";
+  document.querySelector(".loadButton").style.opacity = 0;
+  document.querySelector(".loadButton").style.cursor = "default";
+};
